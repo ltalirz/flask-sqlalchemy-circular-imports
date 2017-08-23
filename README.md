d3a5f170d6295e1b8a7847ba141389bebaf63b10
Leopold Talirz <leopold.talirz@gmail.com>
2017-08-23 12:31:26 +0200

    fix RuntimeError & switch to in-memory sqlite
    Fixed error with flask_sqlalchemy 2.2 (haven't tested with earlier
    versions)
    
        RuntimeError: application not registered on db instance and no
                      application bound to current context

---
+++ README.md
@@ -0,0 +1,5 @@
+Avoiding circular imports using blueprints
+--------------------------------------------------
+
+Minimal working example by S182 on StackOverflow
+https://stackoverflow.com/a/23400668/1069467
--- app.py
+++ app.py
@@ -4,11 +4,10 @@ import os.path
 from views import User
 from views import people
 
-
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
     db.init_app(app)    
     app.register_blueprint(people, url_prefix='')
     return app 
@@ -17,15 +16,14 @@ def create_app():
 def setup_database(app):
     with app.app_context():
         db.create_all()
-    user = User()
-    user.username = "Tom"
-    db.session.add(user)
-    db.session.commit()    
+
+        user = User()
+        user.username = "Tom"
+        db.session.add(user)
+        db.session.commit()    
 
 
 if __name__ == '__main__':
     app = create_app()
-    # Because this is just a demonstration we set up the database like this.
-    if not os.path.isfile('/tmp/test.db'):
-      setup_database(app)
+    setup_database(app)
     app.run()
